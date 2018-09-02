fab670c573d1f82f68bb22f4fefa17bf39aa0c8c
gaolbird <gaolbird@yeah.net>
2018-09-02 08:47:49 +0800

    commit

---
+++ ruanyifeng/module.js
@@ -0,0 +1,10 @@
+// export var firstName = 'Micheal';
+// export var firstName = 'Micheal';
+// export var lastName = 'Jackson';
+// export var year = 1958;
+
+var firstName = 'Micheal';
+var lastName = 'Jackson';
+var year = 1958;
+
+export { firstName, lastName, year };
