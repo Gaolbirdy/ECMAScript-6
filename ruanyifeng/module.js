bf2a6c7ff407dce482a45d79bd4a8d4a15086e72
gaolbird <gaolbird@yeah.net>
2018-09-02 10:23:25 +0800

    commit

--- ruanyifeng/module.js
+++ ruanyifeng/module.js
@@ -3,8 +3,137 @@
 // export var lastName = 'Jackson';
 // export var year = 1958;
 
-var firstName = 'Micheal';
-var lastName = 'Jackson';
-var year = 1958;
+// var firstName = 'Micheal';
+// var lastName = 'Jackson';
+// var year = 1958;
 
-export { firstName, lastName, year };
+// export { firstName, lastName, year };
+
+// export function multiply(x, y) {
+//     return x * y;
+// }
+
+// function v1() { }
+// function v2() { }
+// export {
+//     v1 as streamV1,
+//     v2 as streamV2,
+//     v2 as streamLatestVersion
+// };
+
+// export 1;
+
+// var m = 1;
+// export m;
+
+// export var m = 1;
+
+// var m = 1;
+// export { m };
+
+// var n = 1;
+// export { n as m };
+
+// function f() { }
+// export f;
+
+// export function f() { };
+
+// function f() { };
+// export { f };
+
+// class c { };
+// export c;
+
+// export class c { };
+
+// class c { };
+// export { c };
+
+// export var foo = 'bar';
+// setTimeout(() => foo = 'baz', 500);
+
+// function foo() {
+//     export default 'bar';
+// }
+// foo();
+
+// export function area(r) {
+//     return Math.PI * r * r;
+// }
+
+// export function circumference(r) {
+//     return 2 * maht.pi * r;
+// }
+
+// export default function () {
+//     console.log('foo');
+// }
+
+// export default function foo() {
+//     console.log('foo');
+// }
+
+// function foo() {
+//     console.log('foo');
+// }
+// export default foo;
+
+// export default function crc32() {
+    
+// }
+
+// export function crc32() {
+    
+// }
+
+// function add(x, y) {
+//     return x * y;
+// }
+// export { add as default };
+// export default add;
+
+// export var a = 1;
+
+// var a = 1;
+// export default a;
+
+// export default var a = 1;
+
+// export default 42;
+// export default 'a'
+
+// export 42;
+
+// export default function (obj) {
+    
+// }
+
+// export function each(obj, iterator, context) {
+    
+// }
+
+// export { each as forEach };
+
+// export default class {}
+
+// export * from 'circle';
+// export var e = 2.71828182846;
+// export default function (x) {
+//     return Math.exp(x);
+// }
+
+// export { area as circleArea } from 'circle';
+
+// export const A = 1;
+// export const B = 3;
+// export const C = 4;
+
+// export const db = {
+//     url: 'http://my.couchdbserver.local:5984',
+//     admin_username: 'admin',
+//     adim_password: 'admin password'
+// };
+
+// export const users = ['root', 'admin', 'staff', 'ceo', 'chief', 'moderator'];
+
